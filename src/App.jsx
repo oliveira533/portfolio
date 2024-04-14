eb357c57309f0e79a02139020952f78dbc994de4
oliveira533 <go609127@gmail.com>
2024-04-14 16:18:51 -0300

    Finalizando a tela inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
+import MainPage from './main/main';
 import Nav from './nav/nav';
 
 function App() {
   return (
     <div className="App">
       <Nav/>
+      <MainPage/>
     </div>
   );
 }
--- src/main/main.jsx
+++ src/main/main.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import './main.css';
 
-function Main(){
+function MainPage(){
     return(
-        <main>
-            
+        <main className='main'>
+            <img className='main-img' src="#" alt="" />
+            <div className='main-container'>
+                <div className='main-contianer-line'></div>
+                <label className='main-container-text'>Principais projetos</label>
+                <div className='main-contianer-line'></div>
+            </div>
+            <div className='main-projects'>
+                <div className='main-projects-item'>
+                    <img className='main-projects-item-img' src="#" alt="" />
+                    <label className='main-project-title'><a href='https://github.com/oliveira533/help_elder'><b>Help Elder</b></a></label><br/>
+                    <label className='main-project-desc'>Mobile App</label>
+                </div>
+
+                <div className='main-projects-item'>
+                    <img className='main-projects-item-img' src="#" alt="" />
+                    <label className='main-project-title'><a href='https://github.com/oliveira533/Edu-Inspiration'><b>Edu Inspiration</b></a></label><br/>
+                    <label className='main-project-desc'>Web App</label>
+                </div>
+            </div>
         </main>
     );
 }
 
-export default Main;
+export default MainPage;

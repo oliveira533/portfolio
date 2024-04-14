1a254d81a8c9d40a89e997e62857dbc637806034
oliveira533 <go609127@gmail.com>
2024-04-14 13:36:24 -0300

    iniciando a barra de navegação

---
+++ src/nav/nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './nav.css';
+
+function Nav(){
+    return(
+        <nav className='nav'>
+            <p className='nav-text'>Gustavo de Oliveira</p>
+        </nav>
+    );
+}
+
+export default Nav;
